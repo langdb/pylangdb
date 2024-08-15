ac2761d11be442a1643b1f88a12f14b9a1c15eaa
VG <vivekg342@gmail.com>
2024-08-16 02:31:54 +0800

    feat: init pylangdb

---
+++ tests/client.py
@@ -0,0 +1,46 @@
+import unittest
+import os
+import pandas as pd
+from pylangdb import LangDb, MessageRequest
+
+class TestLangDb(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Set environment variables
+        cls.client_id = os.getenv('LANGDB_CLIENT_ID')
+        cls.client_secret = os.getenv('LANGDB_CLIENT_SECRET')
+
+    def setUp(self):
+        # Initialize LangDb instance
+        self.langdb = LangDb(self.client_id, self.client_secret)
+
+    def test_query_df(self):
+        # Test the query_df method
+        query = "select * from langdb.models"
+        result = self.langdb.query_df(query)
+        print(result)
+        self.assertIsNotNone(result)  # Check that the result is not None
+        self.assertIsInstance(result, pd.DataFrame)  # Assuming the result is a DataFrame
+        self.assertGreater(len(result), 0) 
+
+    def test_invoke_model(self):
+        # Test the invoke_model method
+        msg = MessageRequest(
+            model_name='review',
+            message='You are a terrible product',
+            parameters={},
+            include_history=False
+        )
+        response = self.langdb.invoke_model(msg)
+        self.assertEqual(response, '1')
+
+    # test get_entities
+    def test_get_entities(self):
+        # Test the get_entities method
+        for ent in ['models', 'providers', 'views', 'prompts']:
+            res = self.langdb.get_entities(ent)
+            self.assertIsNotNone(res)  # Check that the result is not None
+            self.assertIsInstance(res, list)  # Assuming the result is a list
+            self.assertGreater(len(res), 0)
+if __name__ == '__main__':
+    unittest.main()
